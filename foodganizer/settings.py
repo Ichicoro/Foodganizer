d36034c876731be53b79785cc75848177b2ae612
Marte Montipo <me@marte.dev>
2021-08-04 12:22:51 +0200

    🪡 Add dropdown menu for profile

--- foodganizer/settings.py
+++ foodganizer/settings.py
@@ -142,3 +142,6 @@ STATIC_ROOT = BASE_DIR / 'website' / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,17 @@ from . import views
 urlpatterns = [
     # ex: /
     path('', views.index, name='index'),
-    path('login', auth_views.LoginView.as_view()),
+    path('login', auth_views.LoginView.as_view(
+        extra_context={
+            'next': '/',
+        },
+    ), name='login'),
+    path('profile', views.profile, name='profile'),
+    path('logout', auth_views.LogoutView.as_view(
+        extra_context={
+            'next': '/',
+        },
+    ), name='logout'),
     # # ex: /polls/5/
     # path('<int:question_id>/', views., name='detail'),
     # # ex: /polls/5/results/
