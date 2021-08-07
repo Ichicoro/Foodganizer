4a2913361963f0d26af41537b47d4fea2554af01
blubo <blu.borghi@gmail.com>
2021-08-07 18:03:44 +0200

    Fix admin panel static files finder

--- foodganizer/settings.py
+++ foodganizer/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder'
 ]
 
