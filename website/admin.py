775b056a9e0526fb1822534860e8a405a3debbaf
Blu Borghi <blu.borghi@gmail.com>
2021-08-17 22:12:39 +0200

    Make migrations, Add models to admin panel

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from website.models import User
+from website.models import Item, Kitchen, PostIt, ShoppingCartItem, StoredItem, User
 
-admin.site.register(User)
+admin.site.register([User, Item, StoredItem, Kitchen, PostIt, ShoppingCartItem])
